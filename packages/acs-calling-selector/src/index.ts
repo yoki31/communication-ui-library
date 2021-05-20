3723914093f9f416f95884686df385b397640637
James Burnside <mail@jamesburnside.com>
2021-05-20 13:21:11 -0700

    wip

--- packages/acs-calling-selector/src/index.ts
+++ packages/acs-calling-selector/src/index.ts
@@ -12,3 +12,16 @@ export { localPreviewSelector } from './localPreviewSelector';
 
 export type { CallingBaseSelectorProps } from './baseSelectors';
 export type { DefaultCallingHandlers } from './handlers/createHandlers';
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// Stub while these are being implemented
+export const useSelector = <SelectorT extends (state: any, props: any) => any>(
+  selector: SelectorT,
+  selectorProps?: Parameters<SelectorT>[1]
+): never => {
+  throw 'stub';
+};
+export const usePropsFor = <Component extends (props: any) => JSX.Element>(component: Component): never => {
+  throw 'stub';
+};
+export type GetSelector<Component> = never;
--- packages/communication-react/src/index.ts
+++ packages/communication-react/src/index.ts
@@ -4,9 +4,69 @@
 export { fromFlatCommunicationIdentifier, toFlatCommunicationIdentifier } from '../../acs-ui-common/src';
 export type { CommonProperties } from '../../acs-ui-common/src';
 
-export * from '../../acs-calling-selector/src';
-export * from '../../acs-chat-selector/src';
 export * from '../../calling-stateful-client/src';
 export * from '../../chat-stateful-client/src';
 export * from '../../react-components/src';
 export * from '../../react-composites/src/index.release';
+
+// export * from '../../acs-calling-selector/src';
+import {
+  GetSelector as GetCallSelector,
+  usePropsFor as useCallPropsFor,
+  useSelector as useCallSelector
+} from '../../acs-calling-selector/src';
+export {
+  createDefaultCallingHandlersForComponent,
+  createDefaultCallingHandlers,
+  mediaGallerySelector,
+  videoGallerySelector,
+  complianceBannerSelector,
+  participantListSelector,
+  localPreviewSelector,
+  cameraButtonSelector,
+  microphoneButtonSelector,
+  optionsButtonSelector,
+  screenShareButtonSelector
+} from '../../acs-calling-selector/src';
+export type { CallingBaseSelectorProps, DefaultCallingHandlers } from '../../acs-calling-selector/src';
+
+// export * from '../../acs-chat-selector/src';
+import {
+  GetSelector as GetChatSelector,
+  usePropsFor as useChatPropsFor,
+  useChatSelector
+} from '../../acs-chat-selector/src';
+export {
+  createDefaultChatHandlers,
+  createDefaultChatHandlersForComponent,
+  ChatClientProvider,
+  useChatClient,
+  ChatThreadClientProvider,
+  useChatThreadClient,
+  useThreadId,
+  useChatSelector,
+  chatThreadSelector,
+  typingIndicatorSelector,
+  sendBoxSelector,
+  chatParticipantListSelector
+} from '../../acs-chat-selector/src';
+
+export type {
+  ChatClientProviderProps,
+  ChatThreadClientProviderProps,
+  ChatBaseSelectorProps,
+  DefaultChatHandlers,
+  AreEqual
+} from '../../acs-chat-selector/src';
+
+// Export merged functions/types
+export const useSelector = <SelectorT extends (state: any, props: any) => any>(
+  selector: SelectorT,
+  selectorProps?: Parameters<SelectorT>[1]
+): any => {
+  return useChatSelector(selector, selectorProps);
+};
+
+export const usePropsFor = <Component extends (props: any) => JSX.Element>(component: Component): any => {
+  return useChatPropsFor(component);
+};
